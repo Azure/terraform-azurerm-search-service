951ea709884b80744852f3e0cf6b687710c8310a
Ubuntu <vaangadi@UbuntuOutsideLab1704.ajf3jfpbqciedfg4vg1bdudjea.gx.internal.cloudapp.net>
2017-12-01 01:33:40 +0000

    Adding test kitchen based testing infrastructure

---
+++ test/integration/fixtures/variables.tf
@@ -0,0 +1,8 @@
+variable "resource_group" {}
+variable "location" {}
+variable "search_name" {}
+variable "sku" {}
+variable "replica_count" {}
+variable "partition_count" {}
+variable "hosting_mode" {}
+variable "tags" {}
